8e4ce9e587b65fac4831cdb398b4c157260892a4
Brian Mitchell <bman4789@gmail.com>
2018-11-23 16:46:04 -0600

    Fix typos, add lots of casks, update install flags

--- brew.sh
+++ brew.sh
@@ -17,11 +17,18 @@ brew upgrade
 brew install bash
 brew install bash-completion2
 
+# Add `/usr/local/bin/bash` to `/etc/shells`
+if ! fgrep -q '/usr/local/bin/bash' /etc/shells; then
+  echo '/usr/local/bin/bash' | sudo tee -a /etc/shells;
+fi;
+
 # Install zsh
 # Note: don’t forget to add `/usr/local/bin/zsh` to `/etc/shells` before
 # running `chsh`.
 brew install zsh
 
+
+# Add `/usr/local/bin/zsh` to `/etc/shells`
 # Switch to using brew-installed zsh as default shell
 if ! fgrep -q '/usr/local/bin/zsh' /etc/shells; then
   echo '/usr/local/bin/zsh' | sudo tee -a /etc/shells;
@@ -29,10 +36,7 @@ if ! fgrep -q '/usr/local/bin/zsh' /etc/shells; then
 fi;
 
 # Install `wget` with IRI support.
-brew install wget --with-iri
-
-# Install GnuPG to enable PGP-signing commits.
-brew install gnupg
+brew install wget
 
 # Install more recent versions of some macOS tools.
 brew install vim --with-override-system-vi
@@ -42,17 +46,17 @@ brew install curl
 
 # Install other useful binaries.
 brew install git
-brew install imagemagick --with-webp
+brew install imagemagick
 brew install ssh-copy-id
 brew install tree
 brew install nmap
 brew install mas
 brew install trash
-brew install imagemagick --with-webp
 brew install youtube-dl
+brew install thefuck
 
 # Install development environments
-brew isntall node
+brew install node
 brew install ruby
 brew install python3
 brew install python@2
@@ -65,6 +69,7 @@ brew cask install 1password
 brew cask install docker
 brew cask install font-fira-code
 brew cask install google-chrome
+brew cask install firefox
 brew cask install mactex
 brew cask install pycharm
 brew cask install slack
@@ -74,7 +79,23 @@ brew cask install transmit
 brew cask install vlc
 brew cask install webstorm
 brew cask install xld
-brew cask install zquartz
+brew cask install xquartz
+brew cask install fantastical
+brew cask install private-internet-access
+brew cask install appcleaner
+brew cask install bettertouchtool
+brew cask install coconutbattery
+brew cask install daisydisk
+brew cask install drivedx
+brew cask install intel-power-gadget
+brew cask install keka
+brew cask install gitup
+brew cask install imageoptim
+brew cask install istumbler
+brew cask install pacifist
+brew cask install gpg-suite
+brew cask install screens
+brew cask install screens-connect
 
 # Remove outdated versions from the cellar.
 brew cleanup
